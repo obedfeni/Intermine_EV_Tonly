f57720f186d7c52cb8e50760bc55a2d9c10e8745
obedfeni <101922090+obedfeni@users.noreply.github.com>
2026-05-19 14:32:49 -0700

    Delete tonly-fleet-clean directory

--- tonly-fleet-clean/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: { bodySizeLimit: '10mb' } },
-  images: { unoptimized: true },
-}
-module.exports = nextConfig
